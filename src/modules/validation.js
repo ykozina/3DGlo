a4be570178931286f598fa805923172b2bea037c
Julia Kozina <ykozina@yandex.ru>
2025-04-19 05:05:54 +0300

    lesson 21 extra

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -7,19 +7,30 @@ const validation = () => {
     const formPhone = document.querySelectorAll('[id$="-phone"]')
     const formMessage = document.querySelectorAll('[id$="-message"]')
 
-    console.log(formMessage);
-
     const fieldCheck = (fieldType, regExp) => {
         fieldType.forEach(field => {
-            field.addEventListener('input', (e) => {
-                e.target.value = e.target.value.replace(regExp, "")
+            field.addEventListener('blur', (e) => {
+                e.target.value = e.target.value.replace(regExp, "");
+                e.target.value = e.target.value.replace(/\s{2,}/gi, " ");
+                e.target.value = e.target.value.replace(/\-{2,}/gi, "-");
+                e.target.value = e.target.value.replace(/^[\s?\-?]/gi, "");
+
+                if (field.getAttribute('type') == 'text') {
+                    const firstLetter = /( |^)(.){1}/gi
+
+                    e.target.value = e.target.value.replace(firstLetter, (str) => {
+                        return str.toUpperCase();
+                    })
+                }
             })
+
+
         })
     }
 
-    fieldCheck(formName, /[^а-яА-Я]/g)
+    fieldCheck(formName, /[^а-яА-Я\s]/g)
     fieldCheck(formEmail, /[^a-zA-Z0-9@\-_\.\!\~\*']/g)
-    fieldCheck(formPhone, /[^0-9\(\)\-]/g)
+    fieldCheck(formPhone, /[^0-9\(\)\-\s]/g)
     fieldCheck(formMessage, /[^а-яА-Я\s\.\-]/g)
 }
 
